5827402487d1aa35c1044ab18bc36d29672cadc3
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-03-29 00:01:36 -0400

    added signup,signin,moreinfo

--- app-react/src/Navbar.js
+++ app-react/src/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Signinmodal from './Signinmodal';
+import Signupmodal from './Signupmodal';
 
 
 class Navbar extends Component {
@@ -20,6 +22,8 @@ class Navbar extends Component {
                     </div>
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav navbar-right">
+                            <li><Signinmodal /> </li>
+                            <li><Signupmodal /></li>
                             <li><a href="#"><span className="nav-links">About</span></a></li>
                             <li><a href="#"><span className="nav-links">Account</span></a></li>
                             <li><a href="#"><span className="nav-links">Cart</span></a></li>
