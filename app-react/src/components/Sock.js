7c7703440ec6e7d940121006c54843443541e1fe
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-03-30 16:54:54 -0400

    Added dynamic data

--- app-react/src/components/Sock.js
+++ app-react/src/components/Sock.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 
-class Footer extends Component {
+class Sock extends Component {
 
     render() {
         return (
             <div className="col-md-3 col-sm-6 hero-feature">
                 <div className="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="" />
+                    <img className="sock-image" src={this.props.image} alt="Sock image" />
                     <div className="caption">
-                        <h3>Sock Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h3>{this.props.name}</h3>
+                        <p>{this.props.description}</p>
+                        <p>{this.props.price}</p>
                         <p>
                             <a href="#" className="btn btn-primary">Add to cart</a> <a href="#" className="btn btn-default">More info</a>
                         </p>
@@ -21,7 +22,7 @@ class Footer extends Component {
     }
 }
 
-export default Footer;
+export default Sock;
 
 
 
