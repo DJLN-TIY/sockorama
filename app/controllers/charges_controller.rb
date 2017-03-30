4f23a968c62570a689636df6abcd443429ab0f98
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-03-30 14:05:36 -0400

    forgot what changed, too lazy to look

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,3 @@
 class ChargesController < ApplicationController
 
-  def new
-  end
-
-def create
-  # Amount in cents
-  @amount = params[:product][:price]
-
-  customer = Stripe::Customer.create(
-    :email => params[:user][:email],
-    :source  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => params[:product][:description],
-    :currency    => 'usd'
-  )
-
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
 end
