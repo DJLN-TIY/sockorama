b83a5aa1ff5bb62afbe8889796dbb9a7561c6847
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-03-31 15:11:38 -0400

    adjusting session serializer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])&.authenticate(params[:password])
       if @user
-        render json: @user, status: 200
+        render json: @user, serializer: UserExtendedSerializer, status: 200
       else
         render json: ["Incorrect username or password."], status: 401
       end
